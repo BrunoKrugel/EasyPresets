95409fd6b41bbcc966088fc0aa35370859bbbb9b
Bruno Krugel <bruno.krugel@ifood.com.br>
2022-12-23 21:39:11 -0300

    Fix deploy

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
-import { Preset } from '../Preset/Preset';
+import { Preset } from '../Preset/preset';
 
 const Projects = () => (
   <Section id="projects">
