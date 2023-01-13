3f54e3ad3f7374dd22349a4c202cca40d456191b
Bruno Krugel <bruno.krugel@ifood.com.br>
2023-01-12 22:59:51 -0300

    Fix presets

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,20 +1,18 @@
 export const projects = [
   {
-    title: 'Presets inspiradas em series',
+    title: 'Presets para praia',
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: 'dark',
     tags: ['R$ 19,90'],
-    source: 'https://github.com/Coders-Of-XDA-OT/covid19-status-android',
-    visit: 'https://project.vipuljha.com/covid/',
+    visit: 'https://instagram.com/easypresets4u',
     id: 0,
   },
   {
     title: 'Presets para selfies',
-    description: "Lenovo SNAPit Camera was one of the finest OEM camera apps back in those days. It had tonnes of unique and amazing features. I ported it to work on almost all devices at that time.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: 'selfie',
     tags: ['R$ 19,90'],
-    source: '#',
-    visit: 'https://forum.xda-developers.com/t/app-port-6-0-lenovo-snapit-camera-5-8-53-for-all-devices.3608065/',
+    visit: 'https://instagram.com/easypresets4u',
     id: 1,
   },
 ];
