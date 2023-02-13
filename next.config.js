64795a564cf91acff464c2d354bb69ea06d6ad8b
Bruno Krugel <brunokrugel@gmail.com>
2023-02-13 14:40:43 -0300

    Add prettier

--- next.config.js
+++ next.config.js
@@ -6,13 +6,13 @@ const withImages = require('next-images')
 // })
 
 module.exports = {
-  reactStrictMode: true,
-  ...withImages(),
-  // ...withBundleAnalyzer(),
-  images: {
-    domains: ['raw.githubusercontent.com'],
-  },
-  future: {
-    webpack5: true
-  }
+    reactStrictMode: true,
+    ...withImages(),
+    // ...withBundleAnalyzer(),
+    images: {
+        domains: ['raw.githubusercontent.com'],
+    },
+    future: {
+        webpack5: true,
+    },
 }
