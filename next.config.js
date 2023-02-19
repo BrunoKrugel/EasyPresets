a2c1bd60ad9616eea20aab15782b890caf995236
Bruno Krugel <brunokrugel@gmail.com>
2023-02-19 02:52:00 -0300

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,22 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const withImages = require('next-images')
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// })
 
 module.exports = {
     reactStrictMode: true,
     ...withImages(),
-    // ...withBundleAnalyzer(),
     images: {
         domains: ['raw.githubusercontent.com'],
         disableStaticImages: true,
     },
+    compress: true,
     experimental: {
         forceSwcTransforms: true,
     },
-    //    future: {
-    //        webpack5: true,
-    //    },
 }
